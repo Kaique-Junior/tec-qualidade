5b518db699deee133e5f7e2fa66e52d745741333
[dyad] <kaiquejuniordasilvaoliveira@gmail.com>
2026-06-06 17:14:59 -0300

    [dyad] Configurações iniciais do ToDo List concluídas - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,52 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import DisciplinaPage from "./pages/DisciplinaPage";
-import NotFound from "./pages/NotFound";
+import TodoPage from "./pages/TodoPage";
+import { useAuth } from "@/contexts/auth/hooks/useAuth";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/disciplina/:slug" element={<DisciplinaPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  const { user, loading } = useAuth();
+
+  // Redirecionar não usuários para login
+  useEffect(() => {
+    if (!user && !loading) {
+      window.location.href = '/login';
+    }
+  }, [user, loading]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            {/* Rota de Login */}
+            <Route path="/login" element={<Login />} />
+            
+            {/* Rotas principais - raiz agora é a tela de introdução */}
+            <Route path="/" element={<Home />} />
+            
+            {/* Rota de disciplinas */}
+            <Route path="/disciplinas" element={<Index />} />
+            
+            {/* Rota de ToDo List */}
+            <Route path="/todo" element={<TodoPage />} />
+            
+            {/* Rota dinâmica para quizzes de disciplina */}
+            <Route path="/disciplina/:slug" element={<DisciplinaPage />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;

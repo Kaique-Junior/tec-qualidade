d470011358c8019cdaf75542e9d6e3ef2f36ce79
[dyad] <kaiquejuniordasilvaoliveira@gmail.com>
2026-06-06 18:03:05 -0300

    [dyad] Rota do TodoPage corrigida e importada - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import DisciplinasPage from "./pages/DisciplinasPage";
+import TodoPage from "./pages/TodoPage";
 import Login from "./pages/Login";
 import DisciplinaPage from "./pages/DisciplinaPage";
 import { useAuth } from "@/contexts/auth/hooks/useAuth";
@@ -58,6 +59,7 @@ const App = () => {
             <Route path="/" element={<Index />} />
             <Route path="/disciplinas" element={<DisciplinasPage />} />
             <Route path="/disciplina/:slug" element={<DisciplinaPage />} />
+            <Route path="/todo" element={<TodoPage />} />
             <Route path="/login" element={<Index />} />
           </Routes>
         </BrowserRouter>
